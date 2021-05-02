1274274f307e0883f04febb53eed86d827318392
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2021-05-02 09:49:51 +0200

    Take arr from input

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@ macro_rules! log {
 //     }
 //     ).join("\n");
 // }
-fn format_matrix(m: [[i32; 9]; 9]) -> String {
+fn format_matrix(m: [[i8; 9]; 9]) -> String {
     let mut owned_string: String = String::new(); // "".to_owned();
                                                   // let borrowed_string: &str = "world";
 
@@ -69,6 +69,12 @@ fn format_matrix(m: [[i32; 9]; 9]) -> String {
 pub fn solve(input: JsValue) -> String {
     utils::set_panic_hook();
 
+    // Take `matrix` from `input`
+    // let some_matrix: [i8; 3] = input.into_serde().unwrap(); // [0; 3]; //Array::new();
+    // let some_matrix: [[i8; 9]; 9] = input.into_serde().unwrap(); // [0; 3]; //Array::new();
+    // some_matrix
+    // log!("some_matrix {:?}", some_matrix);
+
     // let v2 = vec![1; 10];
     // This does not work when trying to run in the browser, maybe when running on CLI: println!("{:?}", v2);
     web_sys::console::log_1(&"Start Nidoran".into());
@@ -80,17 +86,19 @@ pub fn solve(input: JsValue) -> String {
     // let some_matrix: [i32; 3] = [9, 8, 7];
     // log!("some_matrix {:?}", some_matrix);
     // https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F8f%2Fe8%2F82%2F8fe8826266ed01e1e7e5a4ab3e31e9f5.jpg&imgrefurl=https%3A%2F%2Fnl.pinterest.com%2Fpin%2F251920172889908428%2F&tbnid=H03lYq_8QYUVVM&vet=12ahUKEwjL4YLz9J3uAhVHP-wKHY5OA2QQMygAegUIARCpAQ..i&docid=fBfHotkgkE3z2M&w=200&h=200&itg=1&q=very%20easy%20sudoku&ved=2ahUKEwjL4YLz9J3uAhVHP-wKHY5OA2QQMygAegUIARCpAQ
-    let matrix: [[i32; 9]; 9] = [
-        [0, 0, 1, 0, 0, 4, 0, 0, 2],
-        [0, 5, 0, 0, 0, 3, 0, 1, 9],
-        [4, 7, 0, 0, 0, 0, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 2, 0, 7],
-        [0, 0, 4, 0, 9, 0, 8, 0, 0],
-        [8, 0, 6, 0, 3, 0, 0, 0, 0],
-        [2, 0, 0, 0, 0, 0, 0, 6, 8],
-        [1, 8, 0, 2, 0, 0, 0, 4, 0],
-        [5, 0, 0, 3, 0, 0, 9, 0, 0],
-    ];
+    // let matrix: [[i8; 9]; 9] = [
+    //     [0, 0, 1, 0, 0, 4, 0, 0, 2],
+    //     [0, 5, 0, 0, 0, 3, 0, 1, 9],
+    //     [4, 7, 0, 0, 0, 0, 0, 0, 5],
+    //     [0, 0, 0, 0, 8, 0, 2, 0, 7],
+    //     [0, 0, 4, 0, 9, 0, 8, 0, 0],
+    //     [8, 0, 6, 0, 3, 0, 0, 0, 0],
+    //     [2, 0, 0, 0, 0, 0, 0, 6, 8],
+    //     [1, 8, 0, 2, 0, 0, 0, 4, 0],
+    //     [5, 0, 0, 3, 0, 0, 9, 0, 0],
+    // ];
+    // TODO This works, but panics when the input does not contain [[i8; 9]; 9]
+    let matrix: [[i8; 9]; 9] = input.into_serde().unwrap();
     // log!("matrix {:?}", matrix);
     log!("matrix {:?}", format_matrix(matrix));
 
