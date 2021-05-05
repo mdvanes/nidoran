e3f82a9986c73805a66e2fd95afb7117f019fe07
M.D. van Es <es@mdworld.nl>
2021-05-05 08:49:13 +0200

    Try map

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 mod utils;
+extern crate generic_array;
 extern crate serde_json;
 extern crate web_sys;
 
+use generic_array::arr;
+use generic_array::functional::*;
+use generic_array::sequence::*;
+use generic_array::typenum::consts::U1;
+use generic_array::typenum::consts::U2;
+use generic_array::typenum::consts::U9;
+use generic_array::{ArrayLength, GenericArray};
 use wasm_bindgen::prelude::*;
 
 // #[macro_use]
@@ -25,14 +33,88 @@ macro_rules! log {
     }
 }
 
-// #[wasm_bindgen]
+// Pseudo code from JS
 // pub fn printMatrix = (m: )=>{
 //     return m.map(row=>{
 //         return row.map(cell=>cell ? `${cell}` : " ").join(" ");
 //     }
 //     ).join("\n");
 // }
+fn format_matrix_old(m: [[i8; 9]; 9]) -> String {
+    let mut owned_string: String = String::new(); // "".to_owned();
+                                                  // let borrowed_string: &str = "world";
+
+    // for x in &m[0] {
+    //     owned_string.push_str(&(String::new() + &x.to_string() + ", "));
+    // }
+    let mut xcounter = 0;
+
+    for x in &m {
+        xcounter += 1;
+        let mut ycounter = 0;
+        for y in x {
+            ycounter += 1;
+            // owned_string.push_str(&(String::new() + &y.to_string() + ", "));
+            owned_string.push_str(&y.to_string());
+            if ycounter != x.len() {
+                owned_string.push_str(", ");
+            }
+        }
+        if xcounter != m.len() {
+            owned_string.push_str("\n");
+        }
+    }
+
+    // There is no semi colon on the end of this line, making it the "tail", which is returned
+    owned_string.to_string()
+
+    // TODO array.map is experimental? Reimplement this with: map & "join", see JS implementation
+    // let s = &m[0].map(|i| String::new() + &i.to_string() + ", ").collect();
+    // s.to_string()
+}
+
+fn intsToStrings(x: [i8; 2]) -> GenericArray<String, U2> {
+    let z = GenericArray::from_slice(&x);
+    z.map(|y| y.to_string())
+    // z.map(|y| y + 1)
+}
+
+fn toGenArr(arr: &[i8; 9]) -> GenericArray<i8, U9> {
+    *GenericArray::from_slice(arr)
+}
+
+// TODO see https://github.com/fizyk20/generic-array/blob/master/DESIGN.md
+// Note: || func is a closure
+
+// TODO convert i8 to u8?
 fn format_matrix(m: [[i8; 9]; 9]) -> String {
+    // let a = (arr![i32; 1, 2, 3]).map(|x| x + 1);
+    // let b: [i32; 3] = [1, 2, 3];
+    // let c: [i8; 3] = [1, 2, 3];
+    // let b = [i32; c];
+    // let row1: &GenericArray<i8, U9> = GenericArray::from_slice(&m[0]);
+    // // let a = (arr!b).map(|x| x + 1);
+    // let row1_mapped = row1
+    //     .map(|x| x.to_string())
+    //     .fold(String::new(), |acc, next| acc + ", " + &next);
+    // log!("row1_mapped {:?}", row1_mapped);
+
+    // let test_multi_arr: GenericArray<i8, U2> = arr![i8; 1, 2];
+    // let test_multi_arr1: GenericArray<[i8; 2], U2> = arr![[i8; 2]; [1, 2], [3, 4]];
+    // log!("test_multi_arr1 {:?}", test_multi_arr1);
+    // let test_multi_arr1_mapped = test_multi_arr1.map(intsToStrings);
+    // log!("test_multi_arr1_mapped {:?}", test_multi_arr1_mapped);
+
+    let m_gen_arr: &GenericArray<[i8; 9], U9> = GenericArray::from_slice(&m);
+    log!("m_gen_arr {:?}", m_gen_arr);
+    let m_deep_gen_arr = m_gen_arr.map(toGenArr);
+    log!("m_deep_gen_arr {:?}", m_deep_gen_arr);
+    let m_strings = m_deep_gen_arr.map(|row| row.map(|cell| cell.to_string()));
+    log!("m_strings {:?}", m_strings);
+    // let m1_mapped = row1.map(|x| x.map(|y| y.to_string()));
+    // //.fold(String::new(), |acc, next| acc + ", " + &next);
+    // log!("m1_mapped {:?}", m1_mapped);
+
     let mut owned_string: String = String::new(); // "".to_owned();
                                                   // let borrowed_string: &str = "world";
 
@@ -101,6 +183,8 @@ pub fn solve(input: JsValue) -> String {
     let matrix: [[i8; 9]; 9] = input.into_serde().unwrap();
     // log!("matrix {:?}", matrix);
     log!("matrix {:?}", format_matrix(matrix));
+
+    // TODO const matrix = getPossibleValues(initialMatrix);
 
     format_matrix(matrix)
 }
